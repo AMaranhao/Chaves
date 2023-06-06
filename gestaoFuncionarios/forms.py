bd58ae71d7cd07fa6aa49f7a33706972d1a1686a
AMaranhao <alexandreqmj@gmail.com>
2023-06-06 04:35:50 -0300

    Versao 1.00.02
    Foram feitas as seguintes alteraçoes:
    -Campo password com validacao
    -delete de funcionarios e usuarios funcionando juntos
    -update do campo is_active
    -tentativa de deixar unicos:
    email, cpf, matricula e username
        feito para username

--- gestaoFuncionarios/forms.py
+++ gestaoFuncionarios/forms.py
@@ -13,9 +13,26 @@ class FuncionarioForm(ModelForm):
             'Cargo_FK': 'Cargo',
         }
 
+        def clean_username(self):
+            cpf = self.cleaned_data['CPF']
+            if User.objects.filter(CPF=cpf).exists():
+                raise forms.ValidationError('Este CPF já está em uso.')
+            return cpf
+
+        def clean_email(self):
+            matricula = self.cleaned_data['Matricula']
+            if User.objects.filter(Matricula=matricula).exists():
+                raise forms.ValidationError('Esta Matricula já está em uso.')
+            return matricula
+
 
 class UserForm(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}),
+        max_length=150,
+        help_text=None
+    )
     class Meta:
         model = User
         fields = ['password', 'username', 'first_name', 'last_name', 'email']
@@ -27,10 +44,52 @@ class UserForm(ModelForm):
             'email': 'Email Senac'
         }
 
-class UserFormSemSenha(ModelForm):
+        def clean_username(self):
+            username = self.cleaned_data['username']
+            if User.objects.filter(username=username).exists():
+                raise forms.ValidationError('Este nome de usuário já está em uso.')
+            return username
+
+        def clean_email(self):
+            email = self.cleaned_data['email']
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('Este email já está em uso.')
+            return email
+
+class UserFormUpdate(ModelForm):
+    is_active = forms.NullBooleanField(widget=forms.NullBooleanSelect)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}),
+        max_length=150,
+        help_text=None
+    )
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active']
+        labels = {
+            'username': 'Usuário',
+            'first_name': 'Nome',
+            'last_name': 'Sobrenome',
+            'email': 'Email Senac',
+            'is_active':'Ativo'
+        }
+
+        def clean_username(self):
+            username = self.cleaned_data['username']
+            if User.objects.filter(username=username).exists():
+                raise forms.ValidationError('Este nome de usuário já está em uso.')
+            return username
+
+        def clean_email(self):
+            email = self.cleaned_data['email']
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('Este email já está em uso.')
+            return email
+
+class UserFormInativo(ModelForm):
+    class Meta:
+        model = User
+        fields = ['is_active']
         labels = {
             'username': 'Usuário',
             'first_name': 'Nome',
--- gestaoFuncionarios/views.py
+++ gestaoFuncionarios/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import PasswordChangeForm
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.decorators import login_required
 from BancoDeDados.models import FUNCIONARIO
 from django.contrib.auth.models import User
-from .forms import FuncionarioForm, UserForm, UserFormSemSenha
+from .forms import FuncionarioForm, UserForm, UserFormUpdate
 from django.db import transaction
 
 
@@ -21,10 +23,18 @@ def funcionario_new(request):
         form2 = FuncionarioForm(request.POST)
         if form1.is_valid() and form2.is_valid():
             with transaction.atomic():
-                userForm1 = form1.save()
+                userForm1 = form1.save(commit=False)
+                password = form1.cleaned_data['password']
+                try:
+                    validate_password(password, user=userForm1)
+                except ValidationError as e:
+                    form1.add_error('password', e)
+                    return render(request, 'funcionarioForm.html', {'form1': form1, 'form2': form2})
+                userForm1.set_password(password)
+                userForm1.save()
+
                 funcionarioForm2 = form2.save(commit=False)
                 funcionarioForm2.User_FK = userForm1
-                userForm1.save()
                 funcionarioForm2.save()
             return redirect('funcionario_list')
     else:
@@ -44,7 +54,7 @@ def funcionario_update(request, id):
     funcionario = get_object_or_404(FUNCIONARIO, pk=id)
 
     if request.method == 'POST':
-        form1 = UserFormSemSenha(request.POST or None, request.FILES or None, instance=funcionario.User_FK)
+        form1 = UserFormUpdate(request.POST or None, request.FILES or None, instance=funcionario.User_FK)
         form2 = FuncionarioForm(request.POST, instance=funcionario)
 
         if form1.is_valid() and form2.is_valid():
@@ -56,7 +66,7 @@ def funcionario_update(request, id):
 
             return redirect('funcionario_list')
     else:
-        form1 = UserFormSemSenha(instance=funcionario.User_FK)
+        form1 = UserFormUpdate(instance=funcionario.User_FK)
         form2 = FuncionarioForm(instance=funcionario)
 
     context = {
@@ -66,7 +76,7 @@ def funcionario_update(request, id):
 #    return render(request, 'funcionarioForm.html', context)
     return render(request, 'funcionarioFormUpdate.html', context)
 #TODO: Fazer uma view para mudanca de senha
-"""
+
 @login_required
 def change_password(request, id):
     funcionario = get_object_or_404(FUNCIONARIO, pk=id)
@@ -79,7 +89,7 @@ def change_password(request, id):
         form = PasswordChangeForm(user=request.user)
 
     return render(request, 'change_password.html', {'form': form})
-"""
+
 
 @login_required
 def funcionario_delete(request, id):
@@ -87,9 +97,14 @@ def funcionario_delete(request, id):
     form = FuncionarioForm(request.POST or None, request.FILES or None, instance=funcionario)
 
     if request.method == 'POST':
+        user = funcionario.User_FK
         funcionario.delete()
+        user.delete()
         return redirect('funcionario_list')
 
     return render(request, 'funcionarioDeleteConfirm.html', {'form': form})
+
+
+
 
 
