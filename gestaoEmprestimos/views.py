f3e473417a5a63df07e12df559177a6c2add2255
AMaranhao <alexandreqmj@gmail.com>
2023-07-23 11:39:27 -0300

    Versao 1.00.06203 - EMPRESTIMO/AGENDAMENTO
    Mudancas feitas para inserção do app EMPRESTIMOS em templates e na gestão de agendamentos

--- gestaoEmprestimos/views.py
+++ gestaoEmprestimos/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from BancoDeDados.models import EMPRESTIMOS, FUNCIONARIO
-from .forms import EmprestimoForm
+from .forms import EmprestimoForm, CursoEmprestimoForm
 
 @login_required()
 def emprestimo_list(request):
-    emprestimos = EMPRESTIMOS.objects.all()
+    emprestimos = EMPRESTIMOS.objects.filter(Esta_Emprestado = True)
     usuarioLogado = FUNCIONARIO.objects.get(User_FK=request.user)
 
     context = {
@@ -17,17 +17,23 @@ def emprestimo_list(request):
 
 @login_required()
 def emprestimo_new(request):
-    form = EmprestimoForm(request.POST or None, request.FILES or None)
+    form1 = CursoEmprestimoForm(request.POST or None, request.FILES or None)
+    form2 = EmprestimoForm(request.POST or None, request.FILES or None, curso_fk=form1['Curso_FK'].value())
+    funcionario_logado = FUNCIONARIO.objects.get(User_FK=request.user)
 
-    if form.is_valid():
-        form.save()
+    if form2.is_valid():
+        form2.save()
         return redirect('emprestimo_list')
 
     context = {
-        'form': form
+        'form1': form1
+        , 'form2': form2
+        , 'funcionario_logado': funcionario_logado
     }
 
     return render(request, 'emprestimoForm.html', context)
+
+
 
 @login_required()
 def emprestimo_update(request, id):
@@ -60,3 +66,5 @@ def emprestimo_delete(request, id):
     }
 
     return render(request, 'emprestimoDeleteConfirm.html', context)
+
+
